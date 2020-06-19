0413f1f46e2853af0dcefa47f047db8611454a40
Wajidafridi1 <67153071+Wajidafridi1@users.noreply.github.com>
2020-06-19 18:09:25 +0500

    Update zeroboy.py

--- zeroboy.py
+++ zeroboy.py
@@ -47,19 +47,27 @@ def jalan(z):
 
 
 ##### LOGO #####
-logo = """ -----------------------------•◈•
-(  __)\\ ____--------------_------------•◈•
-|__(~)    •||•THE - AAHIL -OFFICAL------•◈•
-|__\~~) •||•RANA - RAJPUT---------------•◈•
-|__(-----\  •◈•------BLACK-TIGER--------•◈•
-|__~~~\ •◈•-----█-------⑦-------█------•◈•
-|__~~~\ •◈•-----█-------⑧-------█------•◈•
-|__~~~\ •◈•-----█-------⑥-------█------•◈•
+logo = """
+     ___       _______ .______       __   _______   __  
+    /   \     |   ____||   _  \     |  | |       \ |  | 
+   /  ^  \    |  |__   |  |_)  |    |  | |  .--.  ||  | 
+  /  /_\  \   |   __|  |      /     |  | |  |  |  ||  | 
+ /  _____  \  |  |     |  |\  \----.|  | |  '--'  ||  | 
+/__/     \__\ |__|     | _| `._____||__| |_______/ |__| 
+                                                        
+---------•|GANDAGER|•-----------
+
+
+
+
+
+
+
 \033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;FlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1; Aahilrna4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/Therana/zero
+\033[1;96mAuthor  \033[1;93m: \033[1;92mgandager302
+\033[1;96mInstagram \033[1;93m: \033[1;gandager302
+\033[1;96mFacebook  \033[1;93m: \033[1; gandager302
+\033[1;96mGithub \033[1;93m: \033[1;92m
 \033[1;91m======================================="""
 
 def tik():
@@ -80,15 +88,15 @@ vuln = "\033[32mVuln"
 os.system("clear")
 print "\033[1;96m ============================================================="
 print  """\033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;92mFlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1;92m Aahilrana4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/Therana/zero
+\033[1;96mAuthor  \033[1;93m: \033[1;92mgandager
+\033[1;96mInstagram \033[1;93m: \033[1;92mgandager
+\033[1;96mFacebook  \033[1;93m: \033[1;92m gandager
+\033[1;96mGithub \033[1;93m: \033[1;92mwajidafridi1
 \033[1;91m======================================="""
 print " \x1b[1;93m============================================================="
 
-CorrectUsername = "rana"
-CorrectPassword = "rana"
+CorrectUsername = "wajid"
+CorrectPassword = "king"
 
 loop = 'true'
 while (loop == 'true'):
@@ -100,10 +108,10 @@ while (loop == 'true'):
             loop = 'false'
         else:
             print "Wrong Password"
-            os.system('xdg-open https://www.Youtube.com/UCsdJQbRf0xpvwaDu1rqgJuA')
+            os.system('xdg-open ')
     else:
         print "Wrong Username"
-        os.system('xdg-open https://www.Youtube.com/UCsdJQbRf0xpvwaDu1rqgJuA')
+        os.system('xdg-')
 
 def login():
 	os.system('clear')
